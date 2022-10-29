d4ffd3706b4f8843bdd5b59f4209cf67336d649c
mariodlcs <mario.sandoval@turnmyapp.com>
2022-10-29 09:58:21 -0500

    init

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cs: ConfigService) => ({
+        type: cs.get<any>('DB_TYPE'),
+        host: cs.get<string>('DB_HOST'),
+        port: cs.get<number>('DB_PORT'),
+        username: cs.get<string>('DB_USERNAME'),
+        password: cs.get<string>('DB_PASSWORD'),
+        database: cs.get<string>('DB_NAME'),
+        entities: [`${__dirname}/**/*.entity.{ts,js}`],
+        synchronize: true,
+        logging: false
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
